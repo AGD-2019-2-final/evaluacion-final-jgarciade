9a4fcd6a328f1c655955ef77a032d584f9b62361
jose garcia <jgarciade@unal.edu.co>
2019-12-08 16:18:20 -0500

    solving q02-10

--- 01-hadoop-50/q02-10/reducer.py
+++ 01-hadoop-50/q02-10/reducer.py
@@ -1,4 +1,19 @@
 import sys
+
 #
 #  >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+if __name__ == "__main__":
+    results = {}
+    for line in sys.stdin:
+        (key, value) = line.split("\t")
+        if not key in results:
+            results[key] = int(value)
+        else:
+            new_value = int(value)
+            if new_value > results[key]:
+                results[key] = new_value
+
+    for (key, value) in sorted(results.items()):
+        sys.stdout.write("{}\t{}\n".format(key, value))
