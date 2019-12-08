4b2751b4ffafc04aab72764f14297c965c7cac17
jose garcia <jgarciade@unal.edu.co>
2019-12-08 18:33:23 -0500

    solving q04-10

--- 01-hadoop-50/q04-10/reducer.py
+++ 01-hadoop-50/q04-10/reducer.py
@@ -1,5 +1,22 @@
 import sys
+import datetime
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 
+if __name__ == "__main__":
+    results = {}
+    for line in sys.stdin:
+        splitted = line.split("\t")
+        (key, date, value) = (splitted[0], splitted[1], splitted[2])
+        date_time_str = "{} 00:00:00.0".format(date)
+        date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
+        new_timestamp = date_time_obj.timestamp()
+        if not key in results:
+            results[key] = {"value": int(value), "timestamp": new_timestamp}
+        else:
+            if new_timestamp < results[key]["timestamp"]:
+                results[key] = {"value": int(value), "timestamp": new_timestamp}
+
+    for (key, value) in sorted(results.items()):
+        sys.stdout.write("{},{}\n".format(key, value["value"]))
