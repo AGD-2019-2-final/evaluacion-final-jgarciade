e69f8f3e53e7b2ea0ec6d61e74787f9d7ac519b4
jose garcia <jgarciade@unal.edu.co>
2019-12-15 22:38:01 -0500

    solving q06-10

--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -2,3 +2,23 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+
+if __name__ == "__main__":
+    results = {}
+    for line in sys.stdin:
+        splitted = line.split("\t")
+        (key, date, value) = (splitted[0], splitted[1], splitted[2])
+        values = results.get(key, None)
+        if values is None:
+            results[key] = {"min": float(value), "max": float(value)}
+            continue
+        new_value = float(value)
+        if new_value < results[key]["min"]:
+            results[key]["min"] = new_value
+            continue
+        if new_value > results[key]["max"]:
+            results[key]["max"] = new_value
+
+    for (key, value) in sorted(results.items()):
+        sys.stdout.write("{}\t{}\t{}\n".format(key, value["max"], value["min"]))
