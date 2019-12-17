c6dd828d0d02ee21114e6d7933f923967ce8dea2
jose garcia <jgarciade@unal.edu.co>
2019-12-17 05:57:12 -0500

    solving q07-10

--- 01-hadoop-50/q07-10/reducer.py
+++ 01-hadoop-50/q07-10/reducer.py
@@ -2,3 +2,18 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__ == "__main__":
+    results = {}
+    for line in sys.stdin:
+        splitted = line.split("\t")
+        (key, date, value) = (splitted[0], splitted[1], splitted[2])
+        values = results.get(key, None)
+        if values is None:
+            results[key] = {int(value): date}
+        else:
+            results[key].update({int(value): date})
+
+    for (key, values) in sorted(results.items()):
+        for value in sorted(values.items()):
+            sys.stdout.write("{}\t{}\t{}\n".format(key, value[1], value[0]))
