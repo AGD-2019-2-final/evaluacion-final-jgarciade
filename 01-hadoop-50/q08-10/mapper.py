f3c59685edc318261d227fca6bab3e244223e60f
jose garcia <jgarciade@unal.edu.co>
2019-12-17 06:04:47 -0500

    solving q08-10

--- 01-hadoop-50/q08-10/mapper.py
+++ 01-hadoop-50/q08-10/mapper.py
@@ -3,3 +3,9 @@ import sys
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 
+if __name__ == "__main__":
+    for line in sys.stdin:
+        for column_value in line.split("\n"):
+            values = column_value.split("   ")
+            if len(values) == 3:
+                sys.stdout.write("{}\t{}\t{}\n".format(values[0], values[1], values[2]))
--- 01-hadoop-50/q08-10/reducer.py
+++ 01-hadoop-50/q08-10/reducer.py
@@ -1,4 +1,19 @@
 import sys
+import statistics
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__ == "__main__":
+    results = {}
+    for line in sys.stdin:
+        splitted = line.split("\t")
+        (key, date, value) = (splitted[0], splitted[1], float(splitted[2]))
+        values = results.get(key, None)
+        if values is None:
+            results[key] = [value]
+        else:
+            results[key].append(value)
+
+    for (key, values) in sorted(results.items()):
+        sys.stdout.write("{}\t{}\t{}\n".format(key, sum(values), statistics.mean(values)))
