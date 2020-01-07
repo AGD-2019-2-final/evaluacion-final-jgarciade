28ab35ee9f01b532099a2c69b23be7a3e87adfb3
jose garcia <jgarciade@unal.edu.co>
2020-01-06 21:36:36 -0500

    solving q10-10

--- 01-hadoop-50/q10-10/mapper.py
+++ 01-hadoop-50/q10-10/mapper.py
@@ -2,4 +2,10 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-        
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        for column_value in line.split("\n"):
+            values = column_value.split("\t")
+            if len(values) == 2:
+                sys.stdout.write("{}\t{}\n".format(values[0], values[1]))
--- 01-hadoop-50/q10-10/reducer.py
+++ 01-hadoop-50/q10-10/reducer.py
@@ -2,3 +2,19 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__ == "__main__":
+    results = {}
+    for line in sys.stdin:
+        splitted = line.split("\t")
+        (key, letters) = (int(splitted[0]), splitted[1].replace("\n", ""))
+        for letter in letters.split(","):
+            values = results.get(letter, None)
+            if values is None:
+                results[letter] = [key]
+                continue
+            results[letter].append(key)
+
+    for (key, values) in sorted(results.items()):
+        values = sorted(values)
+        sys.stdout.write("{}\t{}\n".format(key, ','.join(map(str, values))))
